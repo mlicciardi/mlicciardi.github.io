6f62e442a96157971fcfa4fb31b0e9e9dc5bdeda
Mattia Licciardi <mattia.licciardi@gmail.com>
2020-11-25 02:02:47 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,10 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(($) => {
+  $(".level-bar-inner").css("width", "0");
 
-    $('.level-bar-inner').css('width', '0');
-    
-    $(window).on('load', function() {
-
-        $('.level-bar-inner').each(function() {
-        
-            var itemWidth = $(this).data('level');
-            
-            $(this).animate({
-                width: itemWidth
-            }, 800);
-            
-        });
-
+  $(window).on("load", () => {
+    $(".level-bar-inner").each(() => {
+      var width = $(this).data("level");
+      $(this).animate({ width }, 800);
     });
-   
-    
-
+  });
 });
