0bb5edac844211102a4f56b4680d02f14e62c541
Mattia Licciardi <mattia.licciardi@gmail.com>
2020-12-09 18:28:15 +0100

    update

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,4 +1,4 @@
 window.addEventListener("load", () => {
   var items = [...document.getElementsByClassName("level-bar-inner")];
-  items.forEach((x) => x.style.width = '100%');
+  items.forEach((x) => x.style.width = x.dataset.level);
 });
